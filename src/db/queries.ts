21f7c4909af9fca36c8cd34d62ed38b80a7b5fc6
Ahmed <ahmedsheb2003@yahoo.com>
2024-07-13 09:58:56 +0200

    partially functional

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,14 +1,55 @@
+import { eq } from "drizzle-orm";
 import { db } from "./index.js";
 import {
   InsertPackage,
   InsertUser,
   packagesTable,
+  SelectPackage,
+  SelectUser,
   usersTable,
 } from "./schema.js";
-
+export async function getUser(): Promise<
+  Array<{
+    id: number;
+    firstName: string;
+    username: string;
+    lastName: string | null;
+    address: string;
+    state: string | null;
+    country: string;
+    city: string;
+    expirationDate: Date;
+    email: string | null;
+    phone: number;
+    balance: number;
+    quota: number;
+    packageId: number | null;
+  }>
+> {
+  return db.select().from(usersTable);
+}
 export async function createUser(data: InsertUser) {
   await db.insert(usersTable).values(data);
 }
+export async function getPackage(): Promise<
+  Array<{
+    id: number;
+    quota: number;
+    packageName: string;
+    price: number;
+    speed: number;
+    durationDays: number;
+  }>
+> {
+  return db.select().from(packagesTable);
+}
 export async function createPackage(data: InsertPackage) {
   await db.insert(packagesTable).values(data);
+}
+
+export async function deleteUser(id: SelectUser["id"]) {
+  await db.delete(usersTable).where(eq(usersTable.id, id));
+}
+export async function deletePackage(id: SelectPackage["id"]) {
+  await db.delete(packagesTable).where(eq(packagesTable.id, id));
 }
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,52 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
-import { createPackage, createUser } from "./db/queries.js";
+import {
+  createPackage,
+  createUser,
+  deletePackage,
+  deleteUser,
+  getPackage,
+  getUser,
+} from "./db/queries.js";
 
 app.use(express.json());
 
-console.log("hi");
+console.log("welcome");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const message = JSON.stringify({
+    "/user": {
+      get: "returns all users",
+      post: `insert a user with fields: id: number;
+    firstName: string;
+    username: string;
+    lastName: string | null;
+    address: string;
+    state: string | null;
+    country: string;
+    city: string;
+    expirationDate: Date;
+    email: string | null;
+    phone: number;
+    balance: number;
+    quota: number;
+    packageId: number | null;`,
+    },
+    "/user/:id": { delete: "deletes a user with the specified id" },
+    "/package": {
+      get: "returns all packages",
+      post: `inserts a package with fields: id: number;
+    quota: number;
+    packageName: string;
+    price: number;
+    speed: number;
+    durationDays: number;`,
+    },
+    "/package/:id": { delete: "deletes a package with the specified id" },
+    note: "'| null' means the field is optional",
+  });
+  res.send(message);
 });
 
 app.post("/user", async (req, res) => {
@@ -24,12 +62,33 @@ app.post("/user", async (req, res) => {
   }
   return res.send("User created!");
 });
-app.post("/package", (req, res) => {
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await getUser();
+    return res.send(users);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.status(400).send("specify id");
+  try {
+    await deleteUser(Number(id));
+    return res.send("positive");
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+app.post("/package", async (req, res) => {
   const { durationDays, packageName, price, quota, speed } = req.body;
   if (!durationDays || !packageName || !price || !quota || !speed)
     return res.status(400).send("Missing fields.");
   try {
-    createPackage({ durationDays, packageName, price, quota, speed });
+    await createPackage({ durationDays, packageName, price, quota, speed });
   } catch (error) {
     return res.status(500).send(error);
   }
@@ -37,4 +96,23 @@ app.post("/package", (req, res) => {
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/package", async (req, res) => {
+  try {
+    const packages = await getPackage();
+    return res.send(packages);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+app.delete("/package/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!id) return res.status(400).send("specify id");
+  try {
+    await deletePackage(Number(id));
+    return res.send("positive");
+  } catch (error) {
+    return res.status(500).send(error);
+  }
 });
